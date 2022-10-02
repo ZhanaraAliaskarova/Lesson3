ff70dc07baf7b38d41a0d4f763be5b8922023d01
lskrvzhnr <41797694+lskrvzhnr@users.noreply.github.com>
2022-10-02 18:19:42 +0600

    6.1.8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,14 @@
-package example.myapp
+import example.myapp.Plecostomus
+import example.myapp.Shark
 
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
 
-fun buildAquarium() {
-    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
-    myAquarium.printSize()
-    val myTower = TowerTank(diameter = 25, height = 40)
-    myTower.printSize()
+    println("Shark: ${shark.color}")
+    println("Plecostomus: ${pleco.color}")
 }
 
-fun main() {
-    buildAquarium()
+fun main () {
+    makeFish()
 }
