7c5f15b5e8e7a417db6d74128fba6d6faeda6875
lskrvzhnr <41797694+lskrvzhnr@users.noreply.github.com>
2022-10-02 18:16:20 +0600

    5.2.9

--- src/main/kotlin/Aquarium.kt
+++ src/main/kotlin/Aquarium.kt
@@ -1,6 +1,8 @@
 package example.myapp
 
-open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import java.lang.Math.PI
+
+open class Aquarium(var length: Int = 100, var width: Int = 20, open var height: Int = 40) {
     open var volume: Int
         get() = width * height * length / 1000
         set(value) {
@@ -29,5 +31,17 @@ open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int 
         println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
     }
 
+}
+
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+
+    override var water = volume * 0.8
+    override val shape = "cylinder"
 }
 
