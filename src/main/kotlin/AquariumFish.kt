1a55ef012d2ed6b818e463df1a88bba6fb82f1f6
lskrvzhnr <41797694+lskrvzhnr@users.noreply.github.com>
2022-10-02 18:21:14 +0600

    6.2.4

--- src/main/kotlin/AquariumFish.kt
+++ src/main/kotlin/AquariumFish.kt
@@ -4,10 +4,20 @@ abstract class AquariumFish {
     abstract val color: String
 }
 
-class Shark: AquariumFish() {
+class Shark: AquariumFish(), FishAction {
     override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
 }
 
-class Plecostomus: AquariumFish() {
+class Plecostomus: AquariumFish(), FishAction {
     override val color = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
+}
+
+interface FishAction  {
+    fun eat()
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,10 @@ import example.myapp.Shark
 fun makeFish() {
     val shark = Shark()
     val pleco = Plecostomus()
-
     println("Shark: ${shark.color}")
+    shark.eat()
     println("Plecostomus: ${pleco.color}")
+    pleco.eat()
 }
 
 fun main () {
